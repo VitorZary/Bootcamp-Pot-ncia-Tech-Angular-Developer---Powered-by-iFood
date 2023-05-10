ffe66d180d1624f9676115c12b597f44aa3ef6a8
Vitor Hugo Kuhn Zary <vitorhbts@.com>
2023-05-10 15:08:51 -0300

    Seção: Ganhando Produtividade com a Plataforma Angular Curso: Trabalhando com Componentes Angular

---
+++ Ganhando Produtividade com a Plataforma Angular/Trabalhando com Componentes Angular/Criando cpns/C e reg um cpn/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { MenuComponent } from './components/menu.component';
+
+@NgModule({
+  declarations: [AppComponent, HelloComponent, MenuComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
